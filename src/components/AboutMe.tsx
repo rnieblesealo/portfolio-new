04cb1eb0d297b96f7adeaf32a3fa27e54de1cd5c
Rafael Niebles <rnieblesealo@gmail.com>
2025-02-13 14:51:51 -0500

    add projects

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -7,29 +7,58 @@ export default function AboutMe() {
 
   const aboutMe: AboutMeItem[] = [
     {
-      id: "exp1",
-      text: "I love Heathers!",
+      id: "college",
+      text: "I'm going for a B.S. in Computer Science and a minor in math at UCF!",
+      imgSrc: "/images/ucf.png"
+    },
+    {
+      id: "col",
+      text: "I'm very fortunate to be a legal permanent resident from Colombia! I speak both Spanish and English.",
+      imgSrc: "/images/colombia.png"
+    },
+    {
+      id: "run",
+      text: "I love running! I ran my first half-marathon a bit ago :]",
+      imgSrc: "/images/running.png"
+    },
+    {
+      id: "music",
+      text: "Music is my second passion! Click this widget to check some of my songs out :)",
+      imgSrc: "/images/guitar.png"
+    },
+    {
+      id: "favMovie",
+      text: "My favorite movie of all time is Heathers.",
       imgSrc: "/images/heathers.png"
-    }
+    },
   ]
 
-  const aboutMeElements = aboutMe.map((about) => {
+  const aboutMeElements = aboutMe.map((about, index) => {
+    const rot = `rotateZ(${index % 2 === 0 ? 3 : -1}deg)`
+    const offset = `translateX(${index % 2 === 0 ? 20 : -70}px)`
+
+    const appliedTransforms = `${rot} ${offset}`
+
     return (
-      <li key={about.id} className="flex items-center">
+      <li
+        key={about.id}
+        className={`flex items-center`}
+        style={{ transform: appliedTransforms }}
+      >
         <img
           src={about.imgSrc}
           alt="About me illustration"
           className="bg-black p-4 border-box w-[150px]"
         />
-        <p className="bg-black p-4 font-bold">{about.text}</p>
+        <p className="bg-black p-4 font-bold max-w-[400px]">{about.text}</p>
       </li>
     )
   })
 
   return (
     <>
-      <h2>About Me :]</h2>
-      <ul>
+      <h2 className="mb-7">About Me :]</h2>
+      <ul className="flex flex-col gap-10">
         {aboutMeElements}
       </ul>
     </>
