ba71ae741c045e6050456dc0f00e7d17a9d4e9f1
Rafael Niebles <rafaelniebles2003@gmail.com>
2025-05-03 15:45:24 -0400

    add react to langs

--- data/langs.ts
+++ data/langs.ts
@@ -1,14 +1,15 @@
 const langs: string[] = [
+  "react",
   "typescript",
   "html5",
   "css3",
+  "c",
+  "cplusplus",
+  "csharp",
   "python",
   "java",
   "swift",
   "xcode",
-  "c",
-  "cplusplus",
-  "csharp",
   "unity",
   "postgresql"
 ]
